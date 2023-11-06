ab7a2fc89a9afde67c3798c2e9e0871e45ff0c6f
PinkPanther-ny <alvincny529@gmail.com>
2023-11-06 22:24:18 +0800

    Improve UI and add timeout

--- ChatApp.py
+++ ChatApp.py
@@ -1,5 +1,6 @@
 import json
 import os
+import sys
 import threading
 import tkinter as tk
 import uuid
@@ -27,9 +28,9 @@ class ChatApp:
         Initializes the ChatApp with a specific model and optionally loads a chat history.
 
         :param model: The model to be used for chat completions, default is "gpt-4.0-turbo".
+        :param temperature: The temperature to use for the chat completions.
         :param load_file: Path to a file to load previous chat history, default is an empty string.
         """
-        # The URL of the Flask app server which is running the API
         self.api_url = 'http://openai.fxpyramid.com/interact/'
         self.model = model
         self.temperature = temperature
@@ -58,73 +59,98 @@ class ChatApp:
         # Initialize the total cost to zero
         self.total_cost = 0
 
+        # Set up grid weights for resizing
+        self.root.grid_columnconfigure(0, weight=1)
+        # Set up grid weights for resizing
+        self.root.grid_columnconfigure(0, weight=1)
+        self.root.grid_rowconfigure(4, weight=1)  # This should be the chat_frame's row
+
+        # Other rows should have a weight of 0 or be omitted if they shouldn't resize vertically
+        self.root.grid_rowconfigure(0, weight=0)
+        self.root.grid_rowconfigure(1, weight=0)
+        self.root.grid_rowconfigure(2, weight=0)
+        self.root.grid_rowconfigure(3, weight=0)
+        self.root.grid_rowconfigure(5, weight=0)
+        self.root.grid_rowconfigure(6, weight=0)
+
         # Cost Display Label
         self.cost_label = Label(self.root, text=f"Total Cost: {self.total_cost}")
-        self.cost_label.pack(side=tk.TOP, fill=tk.X)
+        self.cost_label.grid(row=0, column=0, sticky="ew")
 
-        # Frame for History Selection
+        # History Frame
         history_frame = Frame(self.root)
-        history_frame.pack(padx=10, pady=5)
+        history_frame.grid(row=1, column=0, sticky="ew", padx=10, pady=5)
 
         history_label = Label(history_frame, text="Select History File:")
-        history_label.pack(side=tk.LEFT)
+        history_label.grid(row=0, column=0, sticky="w")
 
         history_btn = Button(history_frame, text="Browse", command=self.load_history)
-        history_btn.pack(side=tk.RIGHT)
+        history_btn.grid(row=0, column=1, sticky="e")
 
-        # Frame for Model Selection
+        # Model Frame
         model_frame = Frame(self.root)
-        model_frame.pack(padx=10, pady=5)
+        model_frame.grid(row=2, column=0, sticky="ew", padx=10, pady=5)
 
         model_label = Label(model_frame, text="Select Model:")
-        model_label.pack(side=tk.LEFT)
+        model_label.grid(row=0, column=0, sticky="w")
 
         self.model_var = tk.StringVar(self.root)
         self.model_var.set(self.model)  # default value
 
         model_option = tk.OptionMenu(model_frame, self.model_var, "gpt-4", "gpt-3.5-turbo")
-        model_option.pack(side=tk.RIGHT)
+        model_option.grid(row=0, column=1, sticky="e")
 
         # Temperature Slider
-        self.temperature_scale = Scale(self.root, from_=0, to=2, resolution=0.01, orient=tk.HORIZONTAL,
+        self.temperature_scale = Scale(self.root, from_=0, to=2, resolution=0.01, orient="horizontal",
                                        label="Temperature")
-        self.temperature_scale.set(self.temperature)  # default value
-        self.temperature_scale.pack(fill=tk.X, padx=10, pady=5)
+        self.temperature_scale.set(self.temperature)
+        self.temperature_scale.grid(row=3, column=0, sticky="ew", padx=10, pady=5)
 
-        # Chat Box
-        self.chat_history = tk.Text(self.root, width=80, height=20, wrap=tk.WORD)
-        self.chat_history.pack(padx=10, pady=5)
-        self.chat_history.config(state=tk.DISABLED)
+        # Chat Frame
+        chat_frame = Frame(self.root)
+        chat_frame.grid(row=4, column=0, sticky="nsew", padx=10, pady=5)
+        chat_frame.grid_columnconfigure(0, weight=1)
+        chat_frame.grid_rowconfigure(0, weight=1)
+
+        self.chat_history = tk.Text(chat_frame, width=80, height=20, wrap="word")
+        self.chat_history.grid(row=0, column=0, sticky="nsew")
+        self.chat_history.config(state="disabled")
+
+        scrollbar = tk.Scrollbar(chat_frame, command=self.chat_history.yview)
+        scrollbar.grid(row=0, column=1, sticky="ns")
+        self.chat_history['yscrollcommand'] = scrollbar.set
 
         # User Input
         self.user_input = tk.Entry(self.root)
-        self.user_input.pack(fill=tk.X, padx=10, pady=5)
+        self.user_input.grid(row=5, column=0, sticky="ew", padx=10, pady=5)
         self.user_input.bind("<Return>", self.send_message)
 
-        # Frame for Send and Prepare Buttons
+        # Button Frame
         button_frame = tk.Frame(self.root)
-        button_frame.pack(pady=5)
+        button_frame.grid(row=6, column=0, sticky="ew", padx=10, pady=5)
+        button_frame.grid_columnconfigure(1, weight=1)  # This will push the send button to the right
 
-        # Send Button
-        self.send_button = tk.Button(button_frame, text="Send", command=self.send_message)
-        self.send_button.pack(side=tk.LEFT, padx=(0, 10))  # Add padding to separate the buttons
+        # Prepare Button (remains unchanged on the left)
+        self.prepare_button = tk.Button(button_frame, text="Prepare", command=self.prepare_message, width=7)
+        self.prepare_button.grid(row=0, column=1, padx=5, sticky="w")  # Aligns to the left
 
-        # Prepare Button
-        self.prepare_button = tk.Button(button_frame, text="Prepare", command=self.prepare_message)
-        self.prepare_button.pack(side=tk.LEFT)
+        # Role Selection Frame (remains unchanged next to the prepare button)
+        role_frame = tk.Frame(button_frame)
+        role_frame.grid(row=0, column=0, sticky="w",
+                        padx=5)  # Aligns to the left, in the same column as the prepare button
 
-        # Role Selection
-        role_frame = Frame(self.root)
-        role_frame.pack(padx=10, pady=5)
-
-        role_label = Label(role_frame, text="Select Role:")
-        role_label.pack(side=tk.LEFT)
+        role_label = tk.Label(role_frame, text="Select Role:")
+        role_label.grid(row=0, column=0, sticky="w")
 
         self.role_var = tk.StringVar(self.root)
         self.role_var.set("user")  # default value
 
         role_option = tk.OptionMenu(role_frame, self.role_var, "user", "system")
-        role_option.pack(side=tk.RIGHT)
+        role_option.grid(row=0, column=1, sticky="w")
+
+        # Send Button (move to the right side)
+        self.send_button = tk.Button(button_frame, text="Send", command=self.send_message, width=7)
+        self.send_button.grid(row=0, column=1, sticky="e")  # Now aligns to the right due to column weight
 
         self.update_chat_history(welcome_message, "Program")
 
@@ -139,21 +165,6 @@ class ChatApp:
         except Exception:
             os._exit(1)
 
-    def pretty_print_conversation(self, message, role):
-        if self.chat_history.winfo_exists():
-            self.chat_history.insert(END, f"{role.upper()}: {message}")
-            self.chat_history.yview(END)
-
-    def pretty_print_messages(self):
-        self.chat_history.config(state=tk.NORMAL)
-        self.chat_history.delete('1.0', END)  # Clear the current chat history in the widget
-        for message in self.messages:
-            role = message['role'].upper()
-            content = message['content']
-            self.chat_history.insert(END, f"{role}: {content}\n")
-        self.chat_history.yview(END)
-        self.chat_history.config(state=tk.DISABLED)
-
     def load(self, load_file):
         """Loads chat history from a file.
 
@@ -162,7 +173,12 @@ class ChatApp:
         with open(load_file, "r", encoding='utf-8') as f:
             data = json.load(f)
             self.messages = data
-        self.pretty_print_messages()
+
+        self.chat_history.delete('1.0', END)
+        for message in self.messages:
+            role = message['role'].upper()
+            content = message['content']
+            self.update_chat_history(content, role)
 
     def get_chat_history(self):
         """Returns the chat history as a formatted string.
@@ -189,7 +205,7 @@ class ChatApp:
             "temperature": self.temperature,
             "model": self.model,
         }
-        response = requests.post(self.api_url, json=messages_payload)
+        response = requests.post(self.api_url, json=messages_payload, timeout=300)
         if response.status_code == 200:
             data = response.json()
             return formatted_now + data['response']
@@ -228,11 +244,9 @@ class ChatApp:
         try:
             if message == "~exit":
                 self.save()
-                self.pretty_print_conversation("Session ended.", 'System')
                 os._exit(1)
             elif message == "~save":
                 self.save()
-                self.pretty_print_conversation("(saved)", 'System')
                 return
 
             # Add message to the history
@@ -246,7 +260,7 @@ class ChatApp:
                 "temperature": self.temperature,
                 "model": self.model,
             }
-            response = requests.post(self.api_url, json=messages_payload)
+            response = requests.post(self.api_url, json=messages_payload, timeout=300)
             if response.status_code == 200:
                 data = response.json()
                 # Extract the cost from the response and add it to the total cost
@@ -262,8 +276,11 @@ class ChatApp:
                 self.update_chat_history(data['response'], 'Assistant')
             else:
                 # Handle the error case as before
-                self.pretty_print_conversation(f"Failed to get a response, status code: {response.status_code}",
-                                               'System')
+                self.update_chat_history(f"Failed to get a response, status code: {response.status_code}",
+                                         'Program')
+        except Exception as e:
+            self.update_chat_history(f"Failed to get a response, exception: {str(e)}",
+                                     'Program')
         finally:
             # Re-enable user input and buttons after processing
             self.enable_input_widgets()
@@ -289,7 +306,7 @@ class ChatApp:
     def update_chat_history(self, message, role):
         if self.chat_history.winfo_exists():
             self.chat_history.config(state=tk.NORMAL)
-            self.chat_history.insert(END, f"{role.upper()}: {message}\n")
+            self.chat_history.insert(END, f"{role.upper()}: {message}\n\n ------ \n\n ")
             self.chat_history.yview(END)
             self.chat_history.config(state=tk.DISABLED)
 
